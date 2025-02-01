3ef44487258129c5e9ff197dc67ec3e087c0248d
MonicaVR16 <143565250+MonicaVR16@users.noreply.github.com>
2025-02-01 07:31:32 +0000

    First commit

--- src/Input.java
+++ src/Input.java
@@ -1,1 +1,34 @@
+import java.util.Scanner;
 
+public class Input {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //Get user name
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+
+        //Get user age
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+
+        //Get user weight
+        System.out.print("Enter your weight in kg: ");
+        double weight = scanner.nextDouble();
+
+        //Get user response
+        System.out.print("Are you a smoker? (true or fale): ");
+        boolean isSmoker = scanner.nextBoolean();
+
+        //Display all information back to user
+        System.out.println("\nMedical Form:");
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Weight: " + weight + " kg");
+        System.out.println("Smoker: " + isSmoker);
+
+        scanner.close();
+
+
+    }
+}
